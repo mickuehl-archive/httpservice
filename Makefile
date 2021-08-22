de566e1e190fe4d51801dea757bb6fa30323c33f
Michael Kuehl <hello@txs.vc>
2021-08-22 22:02:42 +0200

    lifted the cdn/caddy implementation from podops.dev legacy

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+TARGET_LINUX = GOARCH=amd64 GOOS=linux
+CONTAINER_REGISTRY = eu.gcr.io/podops
+
 .PHONY: all
 all: test
 
@@ -8,3 +11,11 @@ test:
 .PHONY: test_coverage
 test_coverage:
 	go test `go list ./...` -coverprofile=coverage.txt -covermode=atomic
+
+.PHONY: build_test
+build_test:
+	cd cmd/cdn && go build main.go && rm main
+
+.PHONY: build_cdn
+build_cdn:
+	cd cmd/cdn && ${TARGET_LINUX} go build -o svc main.go && docker build -t ${CONTAINER_REGISTRY}/cdn . && docker push ${CONTAINER_REGISTRY}/cdn
---
+++ internal/cdn/modules/storage.go
@@ -0,0 +1,85 @@
+package modules
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+
+	"github.com/txsvc/stdlib/pkg/observer"
+)
+
+type (
+	StorageModuleImpl struct {
+	}
+)
+
+var (
+	// Interface guards
+	_ caddy.Validator             = (*StorageModuleImpl)(nil)
+	_ caddy.Provisioner           = (*StorageModuleImpl)(nil)
+	_ caddyhttp.MiddlewareHandler = (*StorageModuleImpl)(nil)
+	_ caddyfile.Unmarshaler       = (*StorageModuleImpl)(nil)
+)
+
+func init() {
+	caddy.RegisterModule(StorageModuleImpl{})
+	httpcaddyfile.RegisterHandlerDirective("cdn_storage", parseConfig)
+}
+
+func (m StorageModuleImpl) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+
+	parts := strings.Split(r.RequestURI, "/")
+	if len(parts) > 2 {
+		// this assumes r.RequestURI starts with a "/" e.g. "/16304cda8338/bc982aa5.mp3"
+		prod := parts[1]
+		asset := parts[2]
+		userAgent := r.UserAgent()
+		remoteAddr := r.RemoteAddr
+		contentType := "unknown"
+		contentRange := r.Header.Get("Range")
+		size := "0"
+
+		// track api access for billing etc
+		observer.Meter(context.TODO(), "cdn.storage", "production", prod, "user-agent", userAgent, "remote_addr", remoteAddr, "type", contentType, "range", contentRange, "name", asset, "size", size)
+	}
+
+	//os.Stdout.Write([]byte(r.RequestURI + "\n"))
+	return next.ServeHTTP(w, r)
+}
+
+func (StorageModuleImpl) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.podops",
+		New: func() caddy.Module { return new(StorageModuleImpl) },
+	}
+}
+
+func (m *StorageModuleImpl) Provision(ctx caddy.Context) error {
+	return nil
+}
+
+func (m *StorageModuleImpl) Validate() error {
+	return nil
+}
+
+func (m *StorageModuleImpl) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	/*
+		for d.Next() {
+			if !d.Args(&m.Output) {
+				return d.ArgErr()
+			}
+		}
+	*/
+	return nil
+}
+
+func parseConfig(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var m StorageModuleImpl
+	err := m.UnmarshalCaddyfile(h.Dispenser)
+	return m, err
+}
