c06cd77b5af2a4642c748c6f22aed2cb06aac03e
Michael Kuehl <hello@txs.vc>
2021-11-12 08:26:31 +0100

    added http hearder range parsing function

--- pkg/api/utils.go
+++ pkg/api/utils.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"mime"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/txsvc/stdlib/observer"
 )
@@ -46,4 +48,32 @@ func UnmashalJSONResponse(resp *http.Response, v interface{}, b *[]byte) error {
 
 func LogHttpRequest(ctx context.Context, req *http.Request) {
 	observer.LogWithLevel(observer.LevelInfo, req.RequestURI, "user-agent", req.UserAgent())
+}
+
+// ParseRange extracts a byte range if specified. For specs see
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Range_requests
+func ParseRange(r string) (int64, int64) {
+	if r == "" {
+		return 0, -1 // no range requested
+	}
+	parts := strings.Split(r, "=")
+	if len(parts) != 2 {
+		return 0, -1 // no range requested
+	}
+	// we simply assume that parts[0] == "bytes"
+	ra := strings.Split(parts[1], "-")
+	if len(ra) != 2 { // again a simplification, multiple ranges or overlapping ranges are not supported
+		return 0, -1
+	}
+
+	start, err := strconv.ParseInt(ra[0], 10, 64)
+	if err != nil {
+		return 0, -1
+	}
+	end, err := strconv.ParseInt(ra[1], 10, 64)
+	if err != nil {
+		return 0, -1
+	}
+
+	return start, end - start
 }
